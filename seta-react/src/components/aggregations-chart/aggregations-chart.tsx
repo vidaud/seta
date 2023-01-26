5856442662813b2d17ebaea7408351f4984626e3
AdrianaLleshi9427 <adriana.lleshi94@gmail.com>
2023-01-26 16:27:20 +0100

    add aggregations chart WP

---
+++ seta-react/src/components/aggregations-chart/aggregations-chart.tsx
@@ -0,0 +1,187 @@
+import { useState } from 'react';
+import { Chart } from 'primereact/chart';
+
+const AggregationsChart = () => {
+    const [basicData] = useState({
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'Documents / Year',
+                backgroundColor: '#42A5F5',
+                data: [65, 59, 80, 81, 56, 55, 40]
+            }
+        ]
+    });
+
+    const getLightTheme = () => {
+        let basicOptions = {
+            maintainAspectRatio: false,
+            aspectRatio: .8,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: '#495057'
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef'
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef'
+                    }
+                }
+            }
+        };
+
+        let horizontalOptions = {
+            indexAxis: 'y',
+            maintainAspectRatio: false,
+            aspectRatio: .8,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: '#495057'
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef'
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef'
+                    }
+                }
+            }
+        };
+
+        let stackedOptions = {
+            maintainAspectRatio: false,
+            aspectRatio: .8,
+            plugins: {
+                tooltips: {
+                    mode: 'index',
+                    intersect: false
+                },
+                legend: {
+                    labels: {
+                        color: '#495057'
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    stacked: true,
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef'
+                    }
+                },
+                y: {
+                    stacked: true,
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef'
+                    }
+                }
+            }
+        };
+
+        let multiAxisOptions = {
+            maintainAspectRatio: false,
+            aspectRatio: .8,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: '#495057'
+                    }
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: true
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef'
+                    }
+                },
+                y: {
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                    ticks: {
+                        min: 0,
+                        max: 100,
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef'
+                    }
+                },
+                y1: {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    grid: {
+                        drawOnChartArea: false,
+                        color: '#ebedef'
+                    },
+                    ticks: {
+                        min: 0,
+                        max: 100,
+                        color: '#495057'
+                    }
+                }
+            }
+        };
+
+        return {
+            basicOptions,
+            horizontalOptions,
+            stackedOptions,
+            multiAxisOptions
+        }
+    }
+
+    const { basicOptions } = getLightTheme();
+
+    return (
+        <div>
+            <div className="card">
+                <h5>Vertical</h5>
+                <Chart type="bar" data={basicData} options={basicOptions} />
+            </div>
+        </div>
+    )
+}
+
+export default AggregationsChart;
--- seta-react/src/components/time-range/time-range.tsx
+++ seta-react/src/components/time-range/time-range.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Slider } from 'primereact/slider';
 import { Checkbox } from 'primereact/checkbox';
 import './style.css';
+import AggregationsChart from '../aggregations-chart/aggregations-chart';
 
 const TimeRange = ({onTimeRange}) => {
     const [range, setRange] = useState<any>([1958,2023]);
@@ -19,6 +20,9 @@ const TimeRange = ({onTimeRange}) => {
             <div className="field-checkbox">
                 <Checkbox inputId="include_zero" checked={checked} onChange={e => setChecked(e.checked)} />
                 <label htmlFor="include_zero">Include 0</label>
+            </div>
+            <div>
+                <AggregationsChart />
             </div>
             <div className="p-slider-horizontal">
                 <div className="left-range">{range[0]}</div>  
