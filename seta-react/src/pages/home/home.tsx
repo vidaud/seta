c4d0168a54993a404bb2cb91491e1147b7f573d6
anadelvalle78 <adelvalleclavel@gmail.com>
2023-02-09 13:56:13 +0100

    push of home page with fixed error not committing WP

--- seta-react/src/pages/home/home.tsx
+++ seta-react/src/pages/home/home.tsx
@@ -1,4 +1,4 @@
-// import React from 'react';
+// import React from 'react'; --
 import React, { useState, useEffect } from 'react';
 import { Carousel } from 'primereact/carousel';
 import { CarouselService } from '../../services/CarouselService';
