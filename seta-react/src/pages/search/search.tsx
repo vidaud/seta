d7071559e9677427050af24bffbd52bae4668065
AdrianaLleshi9427 <adriana.lleshi94@gmail.com>
2023-02-21 17:15:02 +0100

    suggestions and similar terms WP

--- seta-react/src/pages/search/search.tsx
+++ seta-react/src/pages/search/search.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './style.css';
 import { InputSwitch } from 'primereact/inputswitch';
 import { Button } from 'primereact/button';
@@ -13,6 +13,9 @@ import { AutoComplete } from 'primereact/autocomplete';
 import { SuggestionsService } from '../../services/corpus/suggestions.service';
 import { SimilarsService } from '../../services/corpus/similars.service';
 import { OntologyListService } from '../../services/corpus/ontology-list.service';
+import { Tree } from 'primereact/tree';
+import { OverlayPanel } from 'primereact/overlaypanel';
+import { InputText } from 'primereact/inputtext';
 
 const Search = () => {
     const [showContent, setShowContent] = useState(false);
@@ -26,22 +29,34 @@ const Search = () => {
     const [suggestedTerms, setSuggestedTerms] = useState<any>(null);
     const [filteredTerms, setFilteredTerms] = useState<any>(null);
     const [similarTerms, setSimilarTerms] = useState<any>(null);
-    const [filteredSimilarTerms, setFilteredSimilarTerms] = useState<any>(null);
     const [ontologyList, setOntologyList] = useState<any>(null);
+    const [treeLeaf, setTreeLeaf] = useState<any>(null);
+    const [selectedNodeKeys2, setSelectedNodeKeys2] = useState<any>(term);
     const [swithToAutocomplete, setSwithToAutocomplete] = useState(false);
+    
     const corpusService = new CorpusService();
     const suggestionsService = new SuggestionsService();
     const similarsService = new SimilarsService();
     const ontologyListService = new OntologyListService();
     let corpusParameters$: Observable<CorpusSearchPayload>;
+
+    const isMounted = useRef(false);
+    const op = useRef<OverlayPanel>(null);
     let cp: CorpusSearchPayload;
+
     const itemsBreadCrumb = [
         {label: 'Search', url: '/seta-ui/search'},
         {label: 'Document List'}
     ];
     const home = { icon: 'pi pi-home', url: '/seta-ui' }
-
     useEffect(() => {
+        if (isMounted) {
+            op.current?.hide();
+        }
+    }, [ selectedNodeKeys2 ]);
+    useEffect(() => {
+        isMounted.current = true;
+
         corpusParameters$?.subscribe((corpusParameters: CorpusSearchPayload) => {
           cp = new CorpusSearchPayload({ ...corpusParameters });
         });
@@ -65,11 +80,41 @@ const Search = () => {
             ontologyListService.retrieveOntologyList(term).then(data => {
                 if (data) {
                     setOntologyList(data);
+                    createTree(data);
                 }
             });
+            console.log(selectedNodeKeys2);
+            console.log(Object.keys(selectedNodeKeys2).length)
         }
     }, [term, typeofSearch, timeRangeValue, swithToAutocomplete]);
 
+    const createTree = (nodes) => {
+        let label, key, children, node_list: any = [];
+        nodes.forEach(node => {
+            label = node[0];
+            key = node[0];
+            node.shift();
+            var children_list: any = [];
+            var obj: any = {};
+            node.forEach((item) => {
+                obj = {'label': item, 'key': item};
+                children_list.push(obj);
+            });
+            children = children_list;
+            let tree_node = {
+                key,
+                label,
+                children
+            }
+            node_list.push(tree_node);
+        });
+        let root;
+        root = {
+                "root": node_list
+                
+        }
+        setTreeLeaf(root.root);
+    }
     const onSwitch = (e) => {
         setSwithToAutocomplete(e.value);
     }
@@ -83,17 +128,6 @@ const Search = () => {
                     setAggregations(data.aggregations);
                 }
             });
-            const groupedTerms = [
-                {
-                    label: 'Similar', code: 'SI',
-                    keywords: similarTerms
-                },
-                {
-                    label: 'Ontology List', code: 'SI',
-                    keywords: ontologyList
-                }
-            ];
-            console.log(groupedTerms);
             setShowContent(true);
         }
         else {
@@ -104,12 +138,6 @@ const Search = () => {
     const searchSuggestions = () => {
         setTimeout(() => {
             setFilteredTerms(suggestedTerms);
-        }, 250);
-    }
-
-    const searchSimilars = () => {
-        setTimeout(() => {
-            setFilteredSimilarTerms(similarTerms);
         }, 250);
     }
 
@@ -146,6 +174,10 @@ const Search = () => {
         <div className="page">
             { showContent ? null : <div>Discover and Link Knowledge in EU Documents</div> }
             <div className="col-8">
+                <div className='switchButton'>
+                    <h5>Switch to related terms search</h5>
+                    <InputSwitch checked={swithToAutocomplete} onChange={onSwitch} />
+                </div>
                 <div className="p-inputgroup">
                     <DialogButton
                       onChange={getDocumentList}
@@ -154,20 +186,35 @@ const Search = () => {
                       onChangeContentVisibility={toggleListVisibility}
                     />
                     { swithToAutocomplete ? 
-                        <AutoComplete
-                            suggestions={similarTerms}
-                            completeMethod={searchSimilars}
+                    <>
+                        <InputText
                             type="search"
-                            value={term}
-                            autoHighlight={true}
+                            aria-haspopup
+                            value={Object.keys(selectedNodeKeys2)}
+                            aria-controls="overlay_panel"
+                            className="select-product-button"
+                            onKeyPress={(e) => op.current?.toggle(e)}
                             placeholder="Type term and/or drag and drop here document"
                             onChange={onChangeTerm}
                         />
+                        <OverlayPanel
+                            ref={op}
+                            showCloseIcon
+                            id="overlay_panel"
+                            style={{ width: "60%", left: "20%"}}
+                            className="overlaypanel-demo"
+                        >
+                            <Tree className='tree-panel' value={treeLeaf} header="Related terms" footer={`Selected terms: ${Object.keys(selectedNodeKeys2).length}`} selectionKeys={selectedNodeKeys2} onSelectionChange={(e) => setSelectedNodeKeys2(e.value)} selectionMode="checkbox" ></Tree>
+                        </OverlayPanel>
+                    </>
                     :
                         <AutoComplete
                             suggestions={filteredTerms}
                             completeMethod={searchSuggestions}
                             type="search"
+                            aria-haspopup
+                            aria-controls="overlay_panel"
+                            className="select-product-button"
                             value={term}
                             autoHighlight={true}
                             placeholder="Type term and/or drag and drop here document"
@@ -175,10 +222,6 @@ const Search = () => {
                         />
                     }
                     <Button label="Search" onClick={onSearch}/>
-                </div>
-                <div className='switchButton'>
-                    <h5>Switch to related terms search</h5>
-                    <InputSwitch checked={swithToAutocomplete} onChange={onSwitch} />
                 </div>
             </div>
             <div>
