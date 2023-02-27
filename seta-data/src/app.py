f113dcdca481a2c4635c3772cbc4c3f1588261f9
Andrei P <pandrei02@gmail.com>
2023-02-27 10:54:32 +0200

    seta-data app refactor
    Clean config file

--- seta-data/src/app.py
+++ seta-data/src/app.py
@@ -1,6 +1,4 @@
 import time
-import logging
-from datetime import datetime
 import shutil
 
 from elasticsearch import Elasticsearch
@@ -13,12 +11,23 @@ import os
 from tqdm import tqdm
 import json
 
-import config
-config = config.ProdConfig()  
+import hashlib as hash
+
+from config import Config
+
+def getsha256(filename):
+    BLOCKSIZE = 65536
+    sha = hash.sha256()
+    with open(filename, 'rb') as f:
+        file_buffer = f.read(BLOCKSIZE)
+        while len(file_buffer) > 0:
+            sha.update(file_buffer)
+            file_buffer = f.read(BLOCKSIZE)
+    return sha.hexdigest()
 
 
-def wait_for_es(config):
-    esh = "http://" + config.ES_HOST + "/_cluster/health?pretty"
+def wait_for_es():
+    esh = f"http://{Config.ES_HOST}/_cluster/health?pretty"
     print('waiting for ES', esh, flush=True)
     try:
         es_session = requests.Session()
@@ -27,102 +36,109 @@ def wait_for_es(config):
         print(res,res.ok, flush=True)
         if res.ok:
             res = json.loads(res.content)
-            print("ElsticSearch...", res['status'], flush=True)
+            print("ElasticSearch...", res['status'], flush=True)
             if res['status'] == 'green' or res['status'] == 'yellow':
                 return
         time.sleep(2)
-        wait_for_es(config)
+        wait_for_es()
     except Exception as e:
         time.sleep(2)
-        wait_for_es(config)
+        wait_for_es()
 
 
-def suggestion_update_job(config):
-    models_path = config.MODELS_PATH
-    if os.path.exists(models_path + config.MODELS_WORD2VEC_FILE_CRC):
-        crc = open(models_path + config.MODELS_WORD2VEC_FILE_CRC, 'r').read()
+def suggestion_update_job():
+    models_path = Config.MODELS_PATH
+    if os.path.exists(models_path + Config.MODELS_WORD2VEC_FILE_CRC):
+        crc = open(models_path + Config.MODELS_WORD2VEC_FILE_CRC, 'r').read()
     else:
-        crc = getsha256(models_path + config.MODELS_WORD2VEC_FILE)
-        f = open(models_path + config.MODELS_WORD2VEC_FILE_CRC,mode='w')
+        crc = getsha256(models_path + Config.MODELS_WORD2VEC_FILE)
+        f = open(models_path + Config.MODELS_WORD2VEC_FILE_CRC,mode='w')
         f.write(crc)
         f.close()
 
     es_session = requests.Session()
 #    es_session.trust_env = True
     
-    index_suggestion = config.INDEX_SUGGESTION
-    es = Elasticsearch("http://" + config.ES_HOST, verify_certs=False, request_timeout=30)
-    resp = es_session.put("http://"+  config.ES_HOST + "/"+index_suggestion+ "?pretty")        
+    index_suggestion = Config.INDEX_SUGGESTION
+    es = Elasticsearch("http://" + Config.ES_HOST, verify_certs=False, request_timeout=30)
+    resp = es_session.put(f"http://{Config.ES_HOST}/{index_suggestion}?pretty")      
+      
     print(resp)
+    
     current_w2v_crc, crc_id = get_crc_from_es(es, index_suggestion)
     if crc != current_w2v_crc:
         try:
             bulk(es, gen_data(crc))
             crc_model = {"crc_model": crc}
-            delete_all_suggestion(current_w2v_crc)
+            
+            if current_w2v_crc:
+                delete_all_suggestion(current_w2v_crc)
+                
             if crc_id:
                 es.update(index=index_suggestion, id=crc_id, doc=crc_model)
             else:
                 es.index(index=index_suggestion, document=crc_model)
-        except:
+        except Exception as e:            
             print("errors on suggestion update. New crc: ", crc)
+            print(e)
             
 def gen_data(crc):
     print("suggestion indexing started")
-    terms_model = KeyedVectors.load(config.MODELS_PATH + config.MODELS_WORD2VEC_FILE, mmap="r")
+    
+    terms_model = KeyedVectors.load(Config.MODELS_PATH + Config.MODELS_WORD2VEC_FILE, mmap="r")
     for word in tqdm(list(terms_model.wv.vocab)):
         phrase = word.replace('_', ' ').lower()
         yield {
-            "_index": config.INDEX_SUGGESTION,
+            "_index": Config.INDEX_SUGGESTION,
             "phrase": phrase,
             "crc": crc
         } 
         
-def delete_all_suggestion(crc):
-    if crc:
-        print("suggestion delete started")
-        query = {"query": {"bool": {"must": [{"match": {"crc.keyword": crc}}]}}}
-        es = Elasticsearch("http://" + config.ES_HOST, verify_certs=False, request_timeout=30)
-        res = es.delete_by_query(index=config.INDEX_SUGGESTION, body=query, wait_for_completion=False)
-        print(res)
-        print("suggestion delete finished")
+def delete_all_suggestion(crc):    
+    print("suggestion delete started")
+    
+    query = {"query": {"bool": {"must": [{"match": {"crc.keyword": crc}}]}}}
+    es = Elasticsearch("http://" + Config.ES_HOST, verify_certs=False, request_timeout=30)
+    res = es.delete_by_query(index=Config.INDEX_SUGGESTION, body=query, wait_for_completion=False)
+    
+    print(res)
+    print("suggestion delete finished")
 
 
-def seta_es_init_map(config):
-        es_session = requests.Session()
+def seta_es_init_map():
+    es_session = requests.Session()
 #        es_session.trust_env = True
-        headers = {"Content-Type": "application/json"}
-        fn = config.MODELS_PATH + config.ES_INIT_DATA_CONFIG_FILE
-        f = open(fn,'r')
-        dataformat = f.read()
-#        dataformat = json.loads(dataformat)
-#        dataformat = json.dumps(dataformat[config["INDEX"][0]])
-        f.close()  
-#        print(config["INDEX"][0])
-        print(dataformat)
-        for indx in config.INDEX:
-          resp = es_session.get("http://"+config.ES_HOST+"/"+indx+ "?pretty")
-          if resp.ok:
+    headers = {"Content-Type": "application/json"}
+    fn = Config.MODELS_PATH + Config.ES_INIT_DATA_CONFIG_FILE
+    
+    f = open(fn,'r')
+    dataformat = f.read()    
+    f.close()  
+    
+    print(dataformat)
+    for indx in Config.INDEX:
+        resp = es_session.get(f"http://{Config.ES_HOST}/{indx}?pretty")
+        if resp.ok:
             print("ElasticSearch index mapping exists: ",indx)
-          else:
-            resp = es_session.put("http://"+config.ES_HOST+"/"+indx+ "?pretty", data=dataformat, headers=headers)        
+        else:
+            resp = es_session.put(f"http://{Config.ES_HOST}/{indx}?pretty", data=dataformat, headers=headers)        
             print (resp.content)
 
-def copy_models_files(config):
-    dst = config.MODELS_PATH + config.ES_INIT_DATA_CONFIG_FILE
-    src = config.MODELS_DOCKER_PATH + config.ES_INIT_DATA_CONFIG_FILE
+def copy_models_files():
+    dst = Config.MODELS_PATH + Config.ES_INIT_DATA_CONFIG_FILE
+    src = Config.MODELS_DOCKER_PATH + Config.ES_INIT_DATA_CONFIG_FILE
     shutil.copyfile(src, dst)
 
-    dst = config.MODELS_PATH + config.MODELS_WORD2VEC_FILE
-    src = config.MODELS_DOCKER_PATH + config.MODELS_WORD2VEC_FILE
+    dst = Config.MODELS_PATH + Config.MODELS_WORD2VEC_FILE
+    src = Config.MODELS_DOCKER_PATH + Config.MODELS_WORD2VEC_FILE
     shutil.copyfile(src, dst)
 
-    dst = config.MODELS_PATH + config.MODELS_WORD2VEC_FILE + ".vectors.npy"
-    src = config.MODELS_DOCKER_PATH + config.MODELS_WORD2VEC_FILE + ".vectors.npy"
+    dst = Config.MODELS_PATH + Config.MODELS_WORD2VEC_FILE + ".vectors.npy"
+    src = Config.MODELS_DOCKER_PATH + Config.MODELS_WORD2VEC_FILE + ".vectors.npy"
     shutil.copyfile(src, dst)
 
-    dst = config.MODELS_PATH + config.MODELS_WORD2VEC_FILE_CRC
-    src = config.MODELS_DOCKER_PATH + config.MODELS_WORD2VEC_FILE_CRC
+    dst = Config.MODELS_PATH + Config.MODELS_WORD2VEC_FILE_CRC
+    src = Config.MODELS_DOCKER_PATH + Config.MODELS_WORD2VEC_FILE_CRC
     shutil.copyfile(src, dst)
 
 def get_crc_from_es(es, index_suggestion):
@@ -139,11 +155,12 @@ def get_crc_from_es(es, index_suggestion):
 
            
 def init():
-    wait_for_es(config)
-    copy_models_files(config)
-    suggestion_update_job(config)
-    seta_es_init_map(config)    
+    wait_for_es()
+    copy_models_files()
+    suggestion_update_job()
+    seta_es_init_map()    
     print("SeTA-ES is initialised.")
 
-
-init()
+ 
+if __name__ == "__main__":
+    init()
