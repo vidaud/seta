05493d2c80b58cae48f560c77511d52de4cc14eb
lucia.noce <lucia.noce@ext.ec.europa.eu>
2023-05-23 11:38:03 +0200

    similar and ontology list accept list of terms (wip)

--- seta-api/seta_api/apis/ontology/ontology.py
+++ seta-api/seta_api/apis/ontology/ontology.py
@@ -14,49 +14,55 @@ term_parser.add_argument('term', required=True)
 
 @ontology_api.route("ontology")
 @ontology_api.doc(description='Return a graph that describes the ontology of the specified term. '
-                    'A set of nodes and relative links are provided.'
-                    'For each node depth, id, size and graph size are returned, '
-                    'depth indicates the depth of the node in the graph, id is the identifier of the term for the node,'
-                    ' size indicates the number of occurrences of the term in the document corpus '
-                    'and graph size is useful to visualize the graph.'
-                    'For each link source, target and value are returned, '
-                    'source indicates the node (its id) from which the link starts, '
-                    'target is the node (its id) linked to source'
-                    'and value is used to visualize the graph.',
-        params={'term': 'The term from which build the ontology graph.'},
-        responses={200: 'Success', 404: 'Not Found Error'},
-        security='apikey')
+                              'A set of nodes and relative links are provided.'
+                              'For each node depth, id, size and graph size are returned, '
+                              'depth indicates the depth of the node in the graph, id is the identifier of the term for the node,'
+                              ' size indicates the number of occurrences of the term in the document corpus '
+                              'and graph size is useful to visualize the graph.'
+                              'For each link source, target and value are returned, '
+                              'source indicates the node (its id) from which the link starts, '
+                              'target is the node (its id) linked to source'
+                              'and value is used to visualize the graph.',
+                  params={'term': 'The term from which build the ontology graph.'},
+                  responses={200: 'Success', 404: 'Not Found Error'},
+                  security='apikey')
 class Ontology(Resource):
     @auth_validator()
     @ontology_api.expect(term_parser)
     def get(self):
         args = term_parser.parse_args()
-        
+
         try:
             graphjs = build_graph(args['term'], current_app=app)
             return jsonify(graphjs)
         except Exception as ex:
             abort(404, str(ex))
-        
+
+
+ontology_list_parser = reqparse.RequestParser()
+ontology_list_parser.add_argument('terms', required=True, action="split")
+
+
 @ontology_api.route("ontology-list")
-@ontology_api.doc(description='Return a list of lists of similar terms that describes the ontology of the specified term. '
-                    'Lists are ranked by the relation strenght to a query term. The first node in each list is'
-                    ' direct relation to query term. The following terms in each sublist have relation to'
-                    ' the first node in a sublist.'
-                    'The result should be interpretd as follows: the first item in each sublist is first level'
-                    ' connection to the query term. The following terms in sublists have second level relation'
-                    ' to the main query term and direct connection to the head of sublist.',
-        params={'term': 'The term from which build the ontology tree.'},
-        responses={200: 'Success', 404: 'Not Found Error'},
-        security='apikey')
+@ontology_api.doc(
+    description='Return a list of lists of similar terms that describes the ontology of the specified term. '
+                'Lists are ranked by the relation strenght to a query term. The first node in each list is'
+                ' direct relation to query term. The following terms in each sublist have relation to'
+                ' the first node in a sublist.'
+                'The result should be interpretd as follows: the first item in each sublist is first level'
+                ' connection to the query term. The following terms in sublists have second level relation'
+                ' to the main query term and direct connection to the head of sublist.',
+    params={'terms': 'The list of terms from which build the ontology tree.'},
+    responses={200: 'Success', 404: 'Not Found Error'},
+    security='apikey')
 class OntologyList(Resource):
     @auth_validator()
-    @ontology_api.expect(term_parser)
+    @ontology_api.expect(ontology_list_parser)
     def get(self):
-        args = term_parser.parse_args()
-        
+        args = ontology_list_parser.parse_args()
+
         try:
-            graphjs = build_tree(args['term'], current_app=app)
+            graphjs = build_tree(args['terms'], current_app=app)
             return jsonify(graphjs)
         except ApiLogicError as aex:
             abort(404, str(aex))
--- seta-api/seta_api/apis/similar/similar.py
+++ seta-api/seta_api/apis/similar/similar.py
@@ -9,26 +9,25 @@ from .similar_logic import get_similar_words
 similar_api = Namespace('seta-api-similar', description='Similar')
 
 similar_parser = reqparse.RequestParser()
-similar_parser.add_argument('term', required=True)
+similar_parser.add_argument('terms', required=True, action="split")
 similar_parser.add_argument('n_term', type=int)
 
 
-@similar_api.doc(description='Given a term, return the 20 most similar terms (semantic similarity). '
-                    'For each term similarity and cardinality '
-                    '(number of occurrences in documents) are reported.',
-        params={'term': 'The original term.',
-                'n_term': 'Number of similar terms to be extracted (default 20).'},
-        responses={200: 'Success', 404: 'Not Found Error'},
-        security='apikey')
+@similar_api.doc(description='Given a term or a list of terms, return the 20 most similar terms (semantic similarity). '
+                             'For each term similarity and cardinality (number of occurrences in documents) are reported.'
+                             'When a list of terms is provided the in the returned list similarity values are set to zero',
+                 params={'terms': 'List of terms.',
+                         'n_term': 'Number of similar terms to be extracted (default 20).'},
+                 responses={200: 'Success', 404: 'Not Found Error'},
+                 security='apikey')
 @similar_api.route("similar")
 class SimilarWords(Resource):
     @auth_validator()
     @similar_api.expect(similar_parser)
     def get(self):
         args = similar_parser.parse_args()
-        
-        try:            
-            words = get_similar_words(args['term'], args['n_term'], current_app=app)
+        try:
+            words = get_similar_words(args['terms'], args['n_term'], current_app=app)
             return jsonify(words)
         except ApiLogicError as aex:
             abort(404, str(aex))
