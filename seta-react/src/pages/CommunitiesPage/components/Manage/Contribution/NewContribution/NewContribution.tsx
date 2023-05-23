dc793165440639644360f9b5392c2b6f05dd7108
AdrianaLleshi9427 <adriana.lleshi94@gmail.com>
2023-05-23 18:06:33 +0200

    remove dublicated Mantine Provider

--- seta-react/src/pages/CommunitiesPage/components/Manage/Contribution/NewContribution/NewContribution.tsx
+++ seta-react/src/pages/CommunitiesPage/components/Manage/Contribution/NewContribution/NewContribution.tsx
@@ -1,25 +1,14 @@
-import { MantineProvider } from '@mantine/core'
-
 import { ContextProvider } from '../StepperPage/context/Context'
 import { MainContainer } from '../StepperPage/MainContainer'
 
 const CreateContribution = () => {
   return (
     <>
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: 'light'
-        }}
-      >
-        <ContextProvider>
-          <div className="page">
-            <MainContainer />
-          </div>
-        </ContextProvider>
-      </MantineProvider>
+      <ContextProvider>
+        <div className="page">
+          <MainContainer />
+        </div>
+      </ContextProvider>
     </>
   )
 }
