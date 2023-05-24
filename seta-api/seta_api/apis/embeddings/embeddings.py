2e45cef3c028adad5407278693a2b40c76785b4c
lucia.noce <lucia.noce@ext.ec.europa.eu>
2023-05-24 10:17:43 +0200

    extract_text api WIP

--- seta-api/seta_api/apis/embeddings/embeddings.py
+++ seta-api/seta_api/apis/embeddings/embeddings.py
@@ -1,27 +1,20 @@
 from flask_restx import Namespace, Resource, reqparse, abort
-from flask import current_app as app, jsonify, request
+from flask import current_app as app, jsonify
 
 from seta_api.infrastructure.auth_validator import auth_validator
-from seta_api.infrastructure.ApiLogicError import ApiLogicError
-
-from subprocess import Popen, PIPE
-import werkzeug
 
 from .embeddings_logic import compute_embeddings
 
 emb_api = Namespace('seta-api-embeddings', description='Embeddings')
 
 parser_file = reqparse.RequestParser()
-parser_file.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
 parser_file.add_argument('text')
+
 
 @emb_api.route("compute_embeddings")
 @emb_api.doc(
-    description='Given a file or a plain text, related embeddings are provided. Embeddings are built using Doc2vec. '
-                'Tika is used to extract text from the provided file. '
-                'If both file and text are provided, function will return text embeddings.',
-    params={'file': 'File to be uploaded from which building embeddings.',
-            'text': 'Plain text from which building embeddings.'},
+    description='Given a file or a plain text, related embeddings are provided. Embeddings are built using Doc2vec.',
+    params={'text': 'Plain text from which building embeddings.'},
     responses={200: 'Success'},
     security='apikey')
 class ComputeEmb(Resource):
@@ -35,23 +28,7 @@ class ComputeEmb(Resource):
                 return jsonify(emb)
             except Exception as ex:
                 abort(404, str(ex))
-            
-        if 'file' in request.files:
-            fin = request.files['file']
-            if fin:
-                fin.seek(0)
-                p = Popen(['java', '-jar', 'tika/tika-app-2.4.1.jar', '--text'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-                parsed_file = p.communicate(input=fin.read())[0]
-                
-                try:
-                    emb = compute_embeddings(parsed_file.decode('utf-8'), current_app=app)
-                    return jsonify(emb)
-                except ApiLogicError as aex:
-                    abort(404, str(aex))
-                except:
-                    app.logger.exception("ComputeEmb->post")
-                    abort(500, "Internal server error")
-                
+
         response = jsonify('No text provided.')
         response.status_code = 400
         return response
