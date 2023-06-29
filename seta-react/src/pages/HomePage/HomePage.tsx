237e64d8af7c23067720f4af199635681ad1ad84
anadelvalle78 <adelvalleclavel@gmail.com>
2023-06-29 17:02:13 +0200

    update of HomePage.tsx with modal get started adding gif image of webapp

--- seta-react/src/pages/HomePage/HomePage.tsx
+++ seta-react/src/pages/HomePage/HomePage.tsx
@@ -1,94 +1,115 @@
-import { useState, useEffect } from 'react'
-import { Carousel } from 'primereact/carousel'
-import { Splitter, SplitterPanel } from 'primereact/splitter'
+import React from 'react'
+import {
+  createStyles,
+  Container,
+  Title,
+  Text,
+  rem,
+  Button,
+  Image,
+  Group,
+  Modal
+} from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import TextTransition, { presets } from 'react-text-transition'
 
-import { CarouselService } from '../../services/CarouselService'
-import './style.css'
+import image from '../../images/background.jpg'
+import image1 from '../../images/search_features.gif'
 
-const carousel = {
-  index: 1, //index which u want to display first
-  direction: null, //direction of the carousel..u need to set it to either 'next' or 'prev' based on user click
-  nextIcon: <span className="pi pi-fw pi-caret-right" />,
-  prevIcon: <span className="pi pi-fw pi-caret-left" />
-}
+const useStyles = createStyles(theme => ({
+  wrapper: {
+    position: 'relative',
+    paddingTop: rem(180),
+    paddingBottom: rem(200),
+    backgroundImage: 'url(' + image + ')',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  },
+
+  title: {
+    fontWeight: 800,
+    fontSize: rem(40),
+    letterSpacing: rem(-1),
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+    color: theme.white,
+    marginBottom: 100,
+    textAlign: 'center',
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`
+  },
+
+  controls: {
+    marginTop: 100,
+    display: 'flex',
+    justifyContent: 'center',
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+
+    [theme.fn.smallerThan('xs')]: {
+      flexDirection: 'column'
+    }
+  },
+
+  control: {
+    height: rem(42),
+    fontSize: theme.fontSizes.md,
+
+    '&:not(:first-of-type)': {
+      marginLeft: theme.spacing.md
+    }
+  }
+}))
+const TEXTS = [
+  "Connect, Share and Learn: Explore SeTA's Thriving Communities",
+  'Revolutionize Document Analysis with SeTA: Visualize Concepts and Track Development Over Time',
+  "Unlock the Power of Textual Data with SeTA's Advanced Text Analysis Techniques"
+]
 
 const HomePage = () => {
-  interface ImageProperties {
-    name: string
-    description: string
-    image: string
-  }
+  const { classes } = useStyles()
+  const [index, setIndex] = React.useState(0)
 
-  const [products, setImages] = useState<ImageProperties[]>([])
-  const carouselService = new CarouselService()
-  const responsiveOptions = [
-    {
-      breakpoint: '1199px',
-      numVisible: 3,
-      numScroll: 3
-    },
-    {
-      breakpoint: '991px',
-      numVisible: 2,
-      numScroll: 2
-    },
-    {
-      breakpoint: '567px',
-      numVisible: 1,
-      numScroll: 1
-    }
-  ]
+  React.useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex(index => index + 1),
+      5000 // every  seconds
+    )
 
-  useEffect(() => {
-    carouselService.getImages().then(data => {
-      setImages(data.slice(0, 3))
-    })
+    return () => clearTimeout(intervalId)
   }, [])
 
-  const productTemplate = product => {
-    return (
-      <div className="card">
-        <h1>{product.name}</h1>
-        <Splitter gutterSize={0} style={{ height: '580px' }}>
-          <SplitterPanel className="align-items-right" size={30}>
-            <p
-              style={{
-                lineHeight: '1.5',
-                whiteSpace: 'pre-wrap',
-                fontSize: '1.3em',
-                marginTop: '7em'
-              }}
-            >
-              {product.description} {'\n'} <a href={product.link}>{product.descLink}</a>
-            </p>
-          </SplitterPanel>
-          <SplitterPanel size={70}>
-            <div className="product-item">
-              <div className="product-item-content">
-                <div className="mb-3">
-                  <img src={`${product.image}`} alt={product.name} className="product-image" />
-                </div>
-              </div>
-            </div>
-          </SplitterPanel>
-        </Splitter>
-      </div>
-    )
-  }
+  const [opened, { open, close }] = useDisclosure(false)
 
   return (
-    <div className="card justify-content-center">
-      <Carousel
-        value={products}
-        numVisible={1}
-        numScroll={1}
-        responsiveOptions={responsiveOptions}
-        className="custom-carousel"
-        circular
-        autoplayInterval={3000}
-        itemTemplate={productTemplate}
-      />
-    </div>
+    <>
+      <Modal opened={opened} onClose={close} withCloseButton={true} size="auto">
+        <h1 className={classes.title}>Hello!</h1>
+        <Image src={image1} alt="Search page" />
+        <br />
+        <input type="checkbox" /> Don't Show me again
+      </Modal>
+      <div className={classes.wrapper}>
+        <Container size={940}>
+          <Title className={classes.title}>
+            <TextTransition springConfig={presets.stiff}>
+              <Text
+                component="span"
+                inherit
+                variant="gradient"
+                gradient={{ from: 'blue', to: 'white' }}
+              >
+                {TEXTS[index % TEXTS.length]}
+              </Text>
+            </TextTransition>
+          </Title>
+          <div className={classes.controls}>
+            <Group position="center">
+              <Button onClick={open}>Get Started</Button>
+            </Group>
+          </div>
+        </Container>
+      </div>
+    </>
   )
 }
 
