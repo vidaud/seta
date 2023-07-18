1174bbab1fc6fe9d6ae863ab6b5f9dd07041bcbd
anadelvalle78 <adelvalleclavel@gmail.com>
2023-07-18 18:29:10 +0200

    update of contact page and faqs page

--- seta-react/src/pages/ContactPage/ContactPage.tsx
+++ seta-react/src/pages/ContactPage/ContactPage.tsx
@@ -1,27 +1,54 @@
-import { Image } from 'primereact/image'
-import { Splitter, SplitterPanel } from 'primereact/splitter'
+import { createStyles, Text, Title, SimpleGrid, Image, rem, Center } from '@mantine/core'
 
-import contact from '../../images/contact_p.jpg'
+import { ContactIconsList } from './components/ContactIcons/ContactIcons'
 
-import './style.css'
+import image from '../../images/contact.jpg'
+
+const useStyles = createStyles(theme => ({
+  wrapper: {
+    minHeight: 600,
+    boxSizing: 'border-box',
+    padding: `calc(${theme.spacing.xl} * 3.5)`,
+
+    [theme.fn.smallerThan('sm')]: {
+      padding: `calc(${theme.spacing.xl} * 2.5)`
+    }
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    color: theme.black,
+    lineHeight: 1
+  },
+
+  description: {
+    color: theme.colors[theme.primaryColor][6],
+
+    [theme.fn.smallerThan('md')]: {
+      maxWidth: '100%'
+    }
+  },
+
+  form: {
+    backgroundColor: theme.white
+  }
+}))
 
 const ContactPage = () => {
+  const { classes } = useStyles()
+
   return (
-    <div className="page">
-      <Splitter gutterSize={0} className="p-splitter">
-        <SplitterPanel>
-          <p className="paragraph">
-            You can also contact us via the
-            <a href="https://webgate.ec.europa.eu/connected/community/jrc/jrc-t-digital-transformation-and-data/t4-data-governance-and-services">
-              {' '}
-              JRC T.4 - data Governance and Services
-            </a>
-          </p>
-        </SplitterPanel>
-        <SplitterPanel>
-          <Image src={contact} alt="Contact" width="600" />
-        </SplitterPanel>
-      </Splitter>
+    <div className={classes.wrapper}>
+      <SimpleGrid cols={2} spacing="sm">
+        <div style={{ alignItems: 'Center' }}>
+          <Title className={classes.title}>Contact us</Title>
+          <br />
+          <ContactIconsList />
+        </div>
+        <div className={classes.form}>
+          <Image src={image} maw={440} mx="auto" radius="xs" />
+        </div>
+      </SimpleGrid>
     </div>
   )
 }
--- seta-react/src/pages/FaqsPage/FaqsPage.tsx
+++ seta-react/src/pages/FaqsPage/FaqsPage.tsx
@@ -1,19 +1,90 @@
 import { useState, useEffect } from 'react'
+import {
+  createStyles,
+  Title,
+  Container,
+  Accordion,
+  ThemeIcon,
+  MantineProvider,
+  getStylesRef,
+  rem
+} from '@mantine/core'
+import { IconPlus } from '@tabler/icons-react'
 import parse from 'html-react-parser'
-import { BreadCrumb } from 'primereact/breadcrumb'
-import { ScrollTop } from 'primereact/scrolltop'
-import { Tree } from 'primereact/tree'
 
 import { FaqsService } from '../../services/FaqsService'
-import './style.css'
+
+const useStyles = createStyles(theme => ({
+  wrapper: {
+    paddingTop: `calc(${theme.spacing.xl} * 2)`,
+    minHeight: rem(820),
+    position: 'relative',
+    color: theme.black
+  },
+
+  title: {
+    color: theme.black,
+    fontSize: 52,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    marginBottom: `calc(${theme.spacing.xl} * 1.5)`
+  },
+
+  item: {
+    backgroundColor: theme.white,
+    borderBottom: 0,
+    borderRadius: theme.radius.md,
+    boxShadow: theme.shadows.lg,
+    overflow: 'hidden'
+  },
+
+  control: {
+    fontSize: theme.fontSizes.lg,
+    padding: `${theme.spacing.lg} ${theme.spacing.xl}`,
+    color: theme.black,
+
+    '&:hover': {
+      backgroundColor: 'transparent'
+    }
+  },
+
+  content: {
+    paddingLeft: theme.spacing.xl,
+    lineHeight: 1.6,
+    color: theme.black
+  },
+
+  icon: {
+    ref: getStylesRef('icon'),
+    marginLeft: theme.spacing.md
+  },
+
+  gradient: {
+    backgroundImage: `radial-gradient(${theme.colors[theme.primaryColor][6]} 0%, ${
+      theme.colors[theme.primaryColor][5]
+    } 100%)`
+  },
+
+  itemOpened: {
+    [`& .${getStylesRef('icon')}`]: {
+      transform: 'rotate(45deg)'
+    }
+  },
+
+  button: {
+    display: 'block',
+    marginTop: theme.spacing.md,
+
+    [theme.fn.smallerThan('sm')]: {
+      display: 'block',
+      width: '100%'
+    }
+  }
+}))
 
 const FaqsPage = () => {
+  const { classes } = useStyles()
   const [nodes, setNodes] = useState<any>()
   const nodeService = new FaqsService()
-
-  const breadCrumbsItems = [{ label: 'Faqs', url: '/faqs' }]
-
-  const home = { icon: 'pi pi-home', url: '/seta-ui' }
 
   const expandNode = (node, _expandedKeys) => {
     if (node.children && node.children.length) {
@@ -36,24 +107,86 @@ const FaqsPage = () => {
 
     return <span style={{ textAlign: 'justify' }}>{data}</span>
   }
+  const seta_api = 'http://localhost/seta-api/api/v1/doc'
+  const comm_api = 'http://localhost/seta-ui/api/v1/communities/doc'
 
   return (
-    <>
-      <BreadCrumb model={breadCrumbsItems} home={home} />
-      <div className="page">
-        <h1 className="headerH">FAQs</h1>
-        <div className="card">
-          <ScrollTop />
-          <div className="col-10">
-            <Tree
-              className="p-fixedTree, p-fixedTreenode-children"
-              value={nodes}
-              nodeTemplate={nodeTemplate}
-            />
-          </div>
-        </div>
+    <MantineProvider inherit theme={{ colorScheme: 'light' }}>
+      <div className={classes.wrapper}>
+        <Container size="sm">
+          <Title align="center" className={classes.title}>
+            Frequently Asked Questions
+          </Title>
+
+          <Accordion
+            chevronPosition="right"
+            defaultValue="community-details"
+            chevronSize={50}
+            variant="separated"
+            disableChevronRotation
+            chevron={
+              <ThemeIcon radius="xl" className={classes.gradient} size={32}>
+                <IconPlus size="1.05rem" stroke={1.5} />
+              </ThemeIcon>
+            }
+          >
+            <Accordion.Item className={classes.item} value="community-details">
+              <Accordion.Control>
+                What kind of rights do you need to have in order to update the Community details?
+              </Accordion.Control>
+              <Accordion.Panel>
+                To update the Community details, you need to have administrator rights of the
+                Community{}
+              </Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item className={classes.item} value="resource">
+              <Accordion.Control>
+                What are the specific details that can be updated in a Community?
+              </Accordion.Control>
+              <Accordion.Panel>
+                It is possible update the 'Title' and 'Description' of the Communities{}
+              </Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item className={classes.item} value="filters">
+              <Accordion.Control>
+                How does the user screen and filter search results in SeTA?
+              </Accordion.Control>
+              <Accordion.Panel>
+                On the left side of the Search results area, there are filters that can be applied
+                to refine the search of the results: by Data Range, Data Sources, Taxonomies, etc.{}
+              </Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item className={classes.item} value="upload-doc">
+              <Accordion.Control>
+                Is it necessary to select the path where the document is located or can we simply
+                drag and drop it?
+              </Accordion.Control>
+              <Accordion.Panel>
+                You have both options to upload documents, by drag and drop or selecting the path
+                where the docuemnt is located {}
+              </Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item className={classes.item} value="api">
+              <Accordion.Control>
+                How can the Communities and Search tool be accessed through API's?
+              </Accordion.Control>
+              <Accordion.Panel>
+                The SeTA REST APIs are created with support of Swagger, you can access through our
+                links:
+                <br />
+                {seta_api}
+                <br />
+                {comm_api}
+              </Accordion.Panel>
+            </Accordion.Item>
+          </Accordion>
+        </Container>
       </div>
-    </>
+    </MantineProvider>
   )
 }
 
