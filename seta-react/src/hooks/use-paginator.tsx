459e8bcf784a80a8f11106eed91f2b2bd26060e4
AdrianaLleshi9427 <adriana.lleshi94@gmail.com>
2023-07-21 11:19:17 +0200

    fixes

--- seta-react/src/hooks/use-paginator.tsx
+++ seta-react/src/hooks/use-paginator.tsx
@@ -9,7 +9,7 @@ type Args = {
   total: number
   perPage: number
   page: number
-  info?: { singular: string; plural?: string; currentPageItems?: number }
+  info?: { singular: string; plural?: string }
   resetPageDependencies?: unknown[]
   scrollDependencies?: unknown[]
   scrollOnPageChange?: boolean
@@ -25,9 +25,9 @@ type Args = {
  *   - `info`: when present, an element displaying the current page info will be returned - it has the following properties:
  *     - `singular`: the singular form of the item name - e.g. "document"
  *     - `plural`: the plural form of the item name; if missing, an 's' is added to the singular
- *     - `currentPageItems`: the number of items in the current page; if missing, the `perPage` value is used
  *   - `resetPageDependencies`: an array of dependencies that, when changed, will reset the page to 1
  *   - `scrollDependencies`: an array of dependencies that, when changed, will scroll the page to the `scrollTargetRef` element
+ *   - `scrollOnPageChange`: whether to scroll the page when the page changes; defaults to `true`
  *   - `onPageChange`: a function that will be called when the page changes
  *
  * @returns an object with the following properties:
@@ -35,8 +35,10 @@ type Args = {
  *   - `info`: the info component, if `info` was given in the argument
  *   - `scrollTargetRef`: a ref that should be passed to the element that will be scrolled to when `scrollDependencies` changes
  *   - `scrollIntoView`: a function that can be called to scroll the page to the `scrollTargetRef` element
+ *   - `scrollableRef`: a ref that should be passed to the element that will be scrolled when `scrollOnPageChange` is `true`;
+ *      if not used, the whole page will be scrolled
  */
-const usePaginator = ({
+const usePaginator = <TScrollable extends HTMLElement | null = null>({
   total,
   perPage,
   page,
@@ -46,7 +48,11 @@ const usePaginator = ({
   scrollOnPageChange = true,
   onPageChange
 }: Args) => {
-  const { scrollIntoView, targetRef: scrollTargetRef } = useScrollIntoView<HTMLDivElement>({
+  const {
+    scrollIntoView,
+    targetRef: scrollTargetRef,
+    scrollableRef
+  } = useScrollIntoView<HTMLDivElement, TScrollable>({
     duration: 200,
     offset: 40
   })
@@ -85,26 +91,31 @@ const usePaginator = ({
   const paginator =
     total > perPage ? (
       <Flex align="center" justify="end" gap="sm">
-        <Pagination total={pagesCount(total, perPage)} value={page} onChange={onPageChange} />
+        <Pagination
+          total={pagesCount(total, perPage)}
+          value={page}
+          siblings={1}
+          onChange={onPageChange}
+        />
       </Flex>
     ) : null
 
   let infoElement: ReactElement | false = false
 
   if (total && info) {
-    const { singular, plural, currentPageItems } = info
+    const { singular, plural } = info
 
     const pageStart = (page - 1) * perPage
-    const pageEnd = pageStart + (currentPageItems ?? perPage)
+    const pageEnd = Math.min(pageStart + perPage, total)
 
     const pageInfo =
-      pageStart + 1 === pageEnd
-        ? ` ${singular} ${pageStart + 1}`
-        : ` ${plural ?? singular}s ${pageStart + 1}-${pageEnd}`
+      pageStart + 1 === pageEnd ? ` ${pageStart + 1}` : ` ${pageStart + 1}-${pageEnd}`
+
+    const totalInfo = total === 1 ? ` ${total} ${singular}` : ` ${total} ${plural ?? singular}s`
 
     infoElement = (
       <Text fz="md" color="gray.7">
-        Showing {pageInfo} of {total}
+        Showing {pageInfo} of {totalInfo}
       </Text>
     )
   }
@@ -112,6 +123,7 @@ const usePaginator = ({
   return {
     scrollIntoView,
     scrollTargetRef,
+    scrollableRef,
     paginator,
     info: infoElement || null
   }
