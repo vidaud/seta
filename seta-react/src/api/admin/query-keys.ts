1e6bd75dbe20bb05c25893f2a031c710d0fab3c0
Andrei Patrascu <andrei.patrascu@ext.ec.europa.eu>
2023-08-04 17:35:38 +0300

    Admin pending requests for resources

--- seta-react/src/api/admin/query-keys.ts
+++ seta-react/src/api/admin/query-keys.ts
@@ -2,4 +2,6 @@ export class AdminQueryKeys {
   public static readonly SidebarQueryKey = ['sidebar-stats']
 
   public static readonly CommunityRequestsQueryKey = ['admin-pending-community-requests']
+
+  public static readonly ResourceRequestsQueryKey = ['admin-pending-resource-requests']
 }
