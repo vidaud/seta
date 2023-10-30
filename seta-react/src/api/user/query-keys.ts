9a57f3938ab9472012359adc162a4617b8bbaa13
AdrianaLleshi9427 <adriana.lleshi94@gmail.com>
2023-10-30 17:27:59 +0100

    manage restricted resources from profile

--- seta-react/src/api/user/query-keys.ts
+++ seta-react/src/api/user/query-keys.ts
@@ -1,4 +1,5 @@
 export class UserQueryKeys {
   public static readonly SetaAccount = ['me/user-info']
   public static readonly RSAKeys = ['me/rsa-keys']
+  public static readonly RestrictedResources = ['me/resources']
 }
