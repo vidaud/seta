1b2ba2970ba53faf689eeb624d85ce66fbb3c766
AdrianaLleshi9427 <adriana.lleshi94@gmail.com>
2023-11-03 14:05:54 +0100

    update notification dropdown and header implementations

--- seta-react/src/components/Header/components/NotificationsDropdown.tsx
+++ seta-react/src/components/Header/components/NotificationsDropdown.tsx
@@ -1,5 +1,7 @@
-import { Text, Badge, Box } from '@mantine/core'
-import { useNavigate } from 'react-router-dom'
+import { Badge, Box, Menu } from '@mantine/core'
+import { IconGitPullRequest, IconMessages } from '@tabler/icons-react'
+import { FiUsers } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 import { useStyles } from '~/pages/SearchWithFilters/components/ApplyFilters/styles'
 
@@ -11,32 +13,39 @@ type Props = {
 
 const NotificationsDropdown = ({ notifications }: Props) => {
   const { classes } = useStyles()
-  const navigate = useNavigate()
-
   const items = notifications
     ?.sort((a, b) => a.priority - b.priority)
     .map(link => (
-      <div key={link.label} className={classes.group}>
-        <Text<'a'>
-          component="a"
-          className={link.priority === 1 ? classes.priority : classes.link}
-          onClick={() => {
-            navigate(
-              link.type === 'pending-invite'
-                ? '/community/invites'
-                : link.type === 'membership-request'
-                ? '/community/membership-requests'
-                : '/admin'
-            )
-          }}
-          key={link.label}
-        >
-          {link.label}
-        </Text>
-        <Badge sx={{ marginTop: '2px' }} variant="filled" size="xs">
+      // eslint-disable-next-line react/no-array-index-key
+
+      <Menu.Item
+        pb="xs"
+        className={link.priority === 1 ? classes.priority : classes.link}
+        style={{ display: 'flex' }}
+        key={link.label}
+        component={Link}
+        icon={
+          link.type === 'pending-invite' ? (
+            <IconMessages size="1rem" />
+          ) : link.type === 'membership-request' ? (
+            <FiUsers size="1rem" />
+          ) : (
+            <IconGitPullRequest size="1rem" color={link.priority === 1 ? '#4169e1' : undefined} />
+          )
+        }
+        to={
+          link.type === 'pending-invite'
+            ? '/community/invites'
+            : link.type === 'membership-request'
+            ? '/community/membership-requests'
+            : '/admin'
+        }
+      >
+        {link.label}
+        <Badge sx={{ marginTop: '2px', float: 'right' }} variant="filled" size="xs" ml="2%">
           {link.count}
         </Badge>
-      </div>
+      </Menu.Item>
     ))
 
   return <Box>{items}</Box>
