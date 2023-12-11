77a283a83520681c1b412d5d321555ce22df61bc
Andrei Patrascu <andrei.patrascu@ext.ec.europa.eu>
2023-12-11 12:44:55 +0200

    Decuple dev composer from default

--- seta-compose/dev-up.bat
+++ seta-compose/dev-up.bat
@@ -1,1 +1,1 @@
-docker compose -f docker-compose.yml -f docker-compose-dev.yml --env-file .env.dev up %*
+docker compose -f docker-compose-dev.yml --env-file .env.dev up %*
--- seta-compose/dev-up.sh
+++ seta-compose/dev-up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose -f docker-compose.yml -f docker-compose-dev.yml --env-file .env.dev up $@
+docker compose -f docker-compose-dev.yml --env-file .env.dev up $@
