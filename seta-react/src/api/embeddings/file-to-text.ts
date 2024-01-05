e7b8ebf7edd5c18f1978a904b0954e28e453bc9a
daniel-jianu-101 <127730071+daniel-jianu-101@users.noreply.github.com>
2024-01-05 09:55:28 +0200

    Split Search & NLP APIs (#396)

--- seta-react/src/api/embeddings/file-to-text.ts
+++ seta-react/src/api/embeddings/file-to-text.ts
@@ -1,40 +1,22 @@
-import { useQuery } from '@tanstack/react-query'
 import type { AxiosRequestConfig } from 'axios'
 
+import { environment } from '~/environments/environment'
+
 import api from '../api'
-import type { FileToTextResponse } from '../types/file-to-text.-types'
 
 const FILE_TO_TEXT_API_PATH = '/file_to_text'
 
-export const queryKey = {
-  root: 'file_to_text',
-  file: (file?: FormData) => [queryKey.root, file]
+const axiosConfig: AxiosRequestConfig = {
+  baseURL: environment.nlpApiRoot
 }
-
-const getFileToText = async (
-  file?: FormData,
-  config?: AxiosRequestConfig
-): Promise<FileToTextResponse> => {
-  if (!file) {
-    return { text: '' }
-  }
-
-  const { data } = await api.post<FileToTextResponse>(`${FILE_TO_TEXT_API_PATH}`, file, config)
-
-  return {
-    text: data.text
-  }
-}
-
-export const useFileToText = (file?: FormData) =>
-  useQuery({ queryKey: queryKey.file(file), queryFn: () => getFileToText(file) })
 
 const retrieveText = async (file: File, config?: AxiosRequestConfig) => {
   const formData = new FormData()
 
   formData.append('file', file)
 
-  const { data } = await api.post<FileToTextResponse>(`${FILE_TO_TEXT_API_PATH}`, formData, {
+  const { data } = await api.post<string>(FILE_TO_TEXT_API_PATH, formData, {
+    ...axiosConfig,
     ...config,
     headers: {
       ...config?.headers,
@@ -42,41 +24,15 @@ const retrieveText = async (file: File, config?: AxiosRequestConfig) => {
     }
   })
 
-  return data.text
+  return data
 }
 
 export const getTextFromFile = async (file?: File, config?: AxiosRequestConfig) => {
   if (!file) {
-    return { text: '' }
+    return ''
   }
 
   const text = await retrieveText(file, config)
 
-  return {
-    text
-  }
-}
-
-export const getTextFromFiles = async (files?: File[], config?: AxiosRequestConfig) => {
-  if (!files) {
-    return { text: '' }
-  }
-
-  const textValues: string[] = []
-
-  for (const file of files) {
-    const text = await retrieveText(file, config)
-
-    textValues.push(
-      text
-        .split('\n')
-        .filter(Boolean)
-        .map(value => `"${value}"`)
-        .join(' ')
-    )
-  }
-
-  return {
-    text: textValues.join(' ')
-  }
+  return text
 }
